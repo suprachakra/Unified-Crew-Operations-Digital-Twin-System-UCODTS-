536aba93a50005939206bdbe1700ad70083b9abc
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-04-18 13:13:47 +0800

    Create PreferentialBidding.jsx

---
+++ frontend/src/pages/Crew/PreferentialBidding.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PreferentialBidding = () => (
+  <div>
+    <h1>Crew Bidding Interface</h1>
+    <p>(Interface for crew to submit scheduling bids and preferences.)</p>
+  </div>
+);
+
+export default PreferentialBidding;
