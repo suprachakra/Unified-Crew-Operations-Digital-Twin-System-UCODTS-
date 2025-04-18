8638e8de19dc4b4eff1ab2dfa95cdba80e6718a7
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-04-18 11:56:07 +0800

    Create StandbyManagement.jsx

---
+++ frontend/src/pages/DisruptionRecovery/StandbyManagement.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StandbyManagement = () => {
+  return (
+    <div>
+      <h2>Standby Crew Management</h2>
+      <p>(This page will list and allow management of standby crew.)</p>
+    </div>
+  );
+};
+
+export default StandbyManagement;
