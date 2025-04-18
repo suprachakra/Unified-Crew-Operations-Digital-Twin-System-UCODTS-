5d3b4dc7718a9cd81b0cb2411dcc375c1a153dc8
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-04-18 11:55:18 +0800

    Create index.jsx

---
+++ frontend/src/pages/DisruptionRecovery/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CrewReassignment from "./CrewReassignment";
+
+const DisruptionRecoveryDashboard = () => (
+  <div>
+    <h1>Disruption Recovery Dashboard</h1>
+    <CrewReassignment />
+    {/* Other components such as StandbyManagement, AccommodationManager, RecoveryMetrics can be added here */}
+  </div>
+);
+
+export default DisruptionRecoveryDashboard;
