5a94b6ce799aa8eca478ccb625b12bd5490e5c66
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-04-18 12:44:49 +0800

    Create Scene.jsx

---
+++ frontend/ar-training/src/scenes/EmergencyProcedures/Scene.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmergencyProceduresScene = () => (
+  <div>
+    <h2>Emergency Procedures Training</h2>
+    <p>This AR/VR scene guides you through simulated emergency protocols.</p>
+    {/* Insert WebXR or 3D simulation here */}
+  </div>
+);
+
+export default EmergencyProceduresScene;
