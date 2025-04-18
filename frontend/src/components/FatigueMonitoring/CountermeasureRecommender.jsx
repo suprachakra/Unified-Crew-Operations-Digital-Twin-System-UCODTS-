5028e9c325b6a532768fc251c209749504034a67
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-04-18 11:49:43 +0800

    Create CountermeasureRecommender.jsx

---
+++ frontend/src/components/FatigueMonitoring/CountermeasureRecommender.jsx
@@ -0,0 +1,10 @@
+ import React from "react";
+
+const CountermeasureRecommender = ({ recommendation }) => (
+  <div>
+    <h4>Recommended Countermeasure</h4>
+    <p>{recommendation}</p>
+  </div>
+);
+
+export default CountermeasureRecommender;
